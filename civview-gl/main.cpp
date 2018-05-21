e3318206ec5f4d53ce965906cc4ef19aabfa1366
dlarudgus20 <dlarudgus20@naver.com>
2018-05-21 18:19:54 +0900

    include path

--- civview-gl/main.cpp
+++ civview-gl/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+#include "pch.h"
 
 int main()
 {
     std::cout << "test output\n";
+    boost::thread([] { std::cout << "thread output\n"; }).join();
     std::cin.get();
 }
---
+++ civview-gl/pch.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <map>
+#include <memory>
+#include <exception>
+#include <stdexcept>
+#include <limits>
+#include <type_traits>
+#include <utility>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <SOIL.h>
+
+#include <boost/thread.hpp>
